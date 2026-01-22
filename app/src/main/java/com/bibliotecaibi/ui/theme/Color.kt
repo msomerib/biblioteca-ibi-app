08dec01c9940570b4df33dd69411af2da3370d42
msomerib <94974225+msomerib@users.noreply.github.com>
2026-01-22 14:55:18 -0300

    Create Color.kt

---
+++ app/src/main/java/com/bibliotecaibi/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.bibliotecaibi.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BluePrimary = Color(0xFF1565C0)
+val BlueSecondary = Color(0xFF42A5F5)
+val BlueDark = Color(0xFF0D47A1)

8457b46c8db51f01a0dd45eb1fb98ccdba6559bc
msomerib <94974225+msomerib@users.noreply.github.com>
2026-01-22 14:45:57 -0300

    Create Routes.kt

---
+++ app/src/main/java/com/bibliotecaibi/ui/navigation/Routes.kt
@@ -0,0 +1,11 @@
+package com.bibliotecaibi.ui.navigation
+
+object Routes {
+    const val LOGIN = "login"
+    const val BRANCH = "branch_select"
+    const val HOME = "home"
+    const val BOOKS = "books"
+    const val MEMBERS = "members"
+    const val LOANS = "loans"
+    const val REPORTS = "reports"
+}

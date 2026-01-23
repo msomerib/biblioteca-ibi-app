d5ea294b46b74602c889dc40352cb6b883b76e86
msomerib <94974225+msomerib@users.noreply.github.com>
2026-01-23 11:23:37 -0300

    Fix syntax error in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
+// app/build.gradle.kts — TOPO DO ARQUIVO
 plugins {
     id("com.android.application") version "8.6.0"
     id("org.jetbrains.kotlin.android") version "1.9.22"
-    id("org.jetbrains.kotlin.kapt") version "1.9.22"
+    id("org.jetbrains.kotlin.kapt") version "1.9.22")
 }
 
 android {
